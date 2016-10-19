7c2e2ce224e438cb337d6b157730ad181f0108a9
FilipOlofsson <te15.filip.olofsson@umea.nti.se>
2016-10-20 00:12:03 +0200

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Game {
+
+	public static String[][] field = new String[][] {
+		{"@", "1", "2", "3"},
+		{"1", " ", " ", " "},
+		{"2", " ", " ", " "},
+		{"3", " ", " ", " "}
+	};
+	
+	
+	
+	public static void main(String[] args) {
+		boolean running = true;
+		boolean crossTurn = true;
+		Scanner scan = new Scanner(System.in);
+		
+		
+		while(running) {
+			if(crossTurn) {
+				System.out.println("Cross turn.");
+				int xToChange = scan.nextInt();
+				int yToChange = scan.nextInt();
+				if(field[yToChange][xToChange] != "X" && field[yToChange][xToChange] != "O")
+					field[yToChange][xToChange] = "X";
+				crossTurn = false;
+				
+			} else {
+				System.out.println("Circle turn.");
+				int xToChange = scan.nextInt();
+				int yToChange = scan.nextInt();
+				if(field[yToChange][xToChange] != "X" && field[yToChange][xToChange] != "O")
+					field[yToChange][xToChange] = "O";
+				crossTurn = true;
+			}
+			draw();
+		}
+	}
+	public static void draw() {
+		for(int y1 = 0; y1 < 4; y1++)
+			System.out.println(field[y1][0] + field[y1][1] + field[y1][2] + field[y1][3]);
+	}
+}
